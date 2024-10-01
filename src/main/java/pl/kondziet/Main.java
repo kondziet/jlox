d7f23c2da970669e60aab3536826b2c4b07ac6a7
kondziet <konrad.dabrowski54@gmail.com>
2024-10-01 17:48:47 +0200

    feat: create simple tui for interpreter entry point

--- src/main/java/pl/kondziet/Main.java
+++ src/main/java/pl/kondziet/Main.java
@@ -1,7 +1,46 @@
 package pl.kondziet;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFromFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFromFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String source = new String(bytes, Charset.defaultCharset());
+        run(source);
+    }
+
+    private static void runPrompt() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            String line = scanner.nextLine();
+            if (line.isBlank()) {
+                break;
+            }
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.scanTokens();
     }
 }
